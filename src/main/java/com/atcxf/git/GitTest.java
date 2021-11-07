c5607263e72824979eadd966d14376c686e27481
Fei <atitcxf@163.com>
2021-11-08 00:45:10 +0800

    first commit

---
+++ src/main/java/com/atcxf/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atcxf.git;
+
+/**
+ * @author cxf
+ * @create 2021-11-08 0:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,Git!");
+    }
+}
